da29f4a4a2d81ce78713d55db0761f8c94535618
isnancahyadi <isnan.arifc@gmail.com>
2023-05-26 23:11:08 +0700

    [ADD] profile page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/js/bootstrap.min.js";
 
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import Profile from "./pages/Profile";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: "/detail",
     element: <Detail />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
   },
 ]);
 
